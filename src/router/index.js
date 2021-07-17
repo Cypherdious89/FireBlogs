fcbd4a1d39fe7eb302fb33bbec0a440aa36c4940
Priyansh-09 <priyanshsingh1@gmail.com>
2021-07-18 01:00:28 +0530

    added route guarding

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-/* jshint esversion: 6 */
+/* jshint esversion: 9 */
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
@@ -12,22 +12,55 @@ import CreatePost from "../views/CreatePost.vue";
 import BlogPreview from "../views/BlogPreview.vue";
 import ViewBlog from "../views/ViewBlog.vue";
 import EditBlog from "../views/EditBlog.vue";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/", name: "Home", component: Home, meta: {title: "Home"} },
-  { path: "/blogs", name: "Blogs", component: Blogs, meta: {title: "Blogs"} },
-  { path: "/login", name: "Login", component: Login, meta: {title: "Login"} },
-  { path: "/register", name: "Register", component: Register, meta: {title: "Register"} },
-  { path: "/forgot-password", name: "ForgotPassword", component: ForgotPassword, meta: {title: "Forgot Password"} },
-  { path: "/profile", name: "Profile", component: Profile, meta: {title: "Profile"} },
-  { path: "/admin", name: "Admin", component: Admin, meta: {title: "Admin"} },
-  { path: "/create-post", name: "CreatePost", component: CreatePost, meta: {title: "Create Post"} },
-  { path: "/post-preview", name: "BlogPreview", component: BlogPreview, meta: {title: "Preview Blog Post"} },
-  { path: "/view-blog/:blogid", name: "ViewBlog", component: ViewBlog, meta: {title: "View Blog Post"} },
-  { path: "/edit-blog/:blogid", name: "EditBlog", component: EditBlog, meta: {title: "Edit Blog"} },
+  { path: "/", name: "Home", component: Home, 
+    meta: {title: "Home", requiresAuth: false} 
+  },
 
+  { path: "/blogs", name: "Blogs", component: Blogs, 
+    meta: {title: "Blogs", requiresAuth: false} 
+  },
+
+  { path: "/login", name: "Login", component: Login, 
+    meta: {title: "Login", requiresAuth: false} 
+  },
+
+  { path: "/register", name: "Register", component: Register, 
+    meta: {title: "Register", requiresAuth: false} 
+  },
+
+  { path: "/forgot-password", name: "ForgotPassword", component: ForgotPassword, 
+    meta: {title: "Forgot Password", requiresAuth: false} 
+  },
+
+  { path: "/profile", name: "Profile", component: Profile, 
+    meta: {title: "Profile", requiresAuth: true} 
+  },
+
+  { path: "/admin", name: "Admin", component: Admin, 
+    meta: {title: "Admin", requiresAuth: true, requiresAdmin: true} 
+  },
+
+  { path: "/create-post", name: "CreatePost", component: CreatePost, 
+    meta: {title: "Create Post", requiresAuth: true} 
+  },
+
+  { path: "/post-preview", name: "BlogPreview", component: BlogPreview, 
+    meta: {title: "Preview Blog Post", requiresAuth: true} 
+  },
+
+  { path: "/view-blog/:blogid", name: "ViewBlog", component: ViewBlog, 
+    meta: {title: "View Blog Post", requiresAuth: false} 
+  },
+
+  { path: "/edit-blog/:blogid", name: "EditBlog", component: EditBlog, 
+    meta: {title: "Edit Blog", requiresAuth: true, requiresAdmin: true} 
+  },
 ];
 
 const router = new VueRouter({
@@ -39,6 +72,28 @@ const router = new VueRouter({
 router.beforeEach((to, _from, next) => {
   document.title = `${to.meta.title} | FireBlog`;
   next();
+});
+
+router.beforeEach(async (to, from, next) => {
+  let user = firebase.auth().currentUser;
+  let admin = null;
+  if (user) {
+    let token = await user.getIdTokenResult();
+    admin = token.claims.admin;
+  }
+  if (to.matched.some((res) => res.meta.requiresAuth)) {
+    if (user) {
+      if (to.matched.some((res) => res.meta.requiresAdmin)) {
+        if (admin) {
+          return next();
+        }
+        return next({ name: "Home" });
+      }
+      return next();
+    }
+    return next({ name: "Home" });
+  }
+  return next();
 });
 
 export default router;
